6cf86534d701ef19f64af60914efa08875976a84
tirathpatel <tirath_24@hotmail.com>
2016-03-26 12:23:52 -0400

    fix

--- content.js
+++ content.js
@@ -1,12 +1,4 @@
-
-alert("hello");
 document.body.style.backgroundImage = "url('http://i.kinja-img.com/gawker-media/image/upload/s--rUJaSzPA--/1464290996877758537.jpg')";
-
 alert("Remember to MAKE WATERLOO GREAT AGAIN!");
 
-<<<<<<< HEAD
-=======
-document.body.style.background = 'yellow';
-alert("MAKE WATERLOO GREAT AGAIN!")
->>>>>>> origin/main
 // content.js

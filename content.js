1b56d6589a3a464f3ce28ea1d84744ccc4357090
tirathpatel <tirath_24@hotmail.com>
2016-03-26 12:17:53 -0400

    new feature

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 
 alert("hello");
+document.body.style.backgroundImage = "url('http://i.kinja-img.com/gawker-media/image/upload/s--rUJaSzPA--/1464290996877758537.jpg')";
 
-document.body.style.background = 'yellow';
-alert("Remeber to MAKE WATERLOO GREAT AGAIN!")
+alert("Remember to MAKE WATERLOO GREAT AGAIN!");
+
 // content.js

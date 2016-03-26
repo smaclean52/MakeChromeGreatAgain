bda2b5540be07a9516eadf1db1fbf14f9027c193
tirathpatel <tirath_24@hotmail.com>
2016-03-26 12:58:30 -0400

    new feature

--- content.js
+++ content.js
@@ -2,3 +2,9 @@ document.body.style.backgroundImage = "url('http://i.kinja-img.com/gawker-media/
 alert("Remember to MAKE WATERLOO GREAT AGAIN!");
 
 // content.js
+
+var currentLocation = (window.location.href).toLowerCase();
+if (((currentLocation.indexOf("hotel") != -1) || (currentLocation.indexOf("inn") != -1)) &&
+	(currentLocation.indexOf("trump") == -1)) {
+	window.location = "http://www.trump.com/hotel-collection/";
+}

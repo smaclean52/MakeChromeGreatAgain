0f26be1ec3b458f14f5136a39cbc66904d088138
tirathpatel <tirath_24@hotmail.com>
2016-03-26 18:16:15 -0400

    FINAL

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.body.style.backgroundImage = "url('http://i.kinja-img.com/gawker-media/image/upload/s--rUJaSzPA--/1464290996877758537.jpg')";
-alert("Remember to MAKE WATERLOO GREAT AGAIN!");
+alert("MAKE WATERLOO GREAT AGAIN!");
 
 // content.js
 

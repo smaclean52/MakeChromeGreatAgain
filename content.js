27a7e42bd7b9367dedf28da19fbaf0112cdfd54a
tirathpatel <tirath_24@hotmail.com>
2016-03-26 16:08:15 -0400

    e

--- content.js
+++ content.js
@@ -31,26 +31,21 @@ if (((currentLocation.indexOf("hotel") != -1) || (currentLocation.indexOf("inn")
   	}
   }
 
-  	
+  	/*
   	for (i = 0; i < searchP.length; i++)
   	{
   	if (searchP[i].innerHTML.search("China") != -1) {
-  		
-    var max = 20;
-    var min = 1;
-    var myAudio = new Audio();        // create the audio object
-    myAudio.src = "sounds/"+(Math.floor(Math.random() * (max - min + 1)) + min ) +".mp3";
-      /*
+      
   		var myAudio = new Audio();  
             // create the audio object
 	myAudio.src = "sounds/china.mp3"; // assign the audio file to it
 	myAudio.play();
-  */
-  alert("CHINA");
+  //alert("CHINA");
 		break;
   	}
-
-  }
+    }
+*/
+  
 
 
 	

b8b980669ad6ab77113ae57c370c2540bfc6788e
Vlad Cojanu <cojanu.vlad@gmail.com>
2016-03-26 12:09:37 -0400

    addedSounds

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.tabson.Created.addListener(function(tab) {
   // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+ alert("Listener triggered");
+  var myAudio = new Audio();        // create the audio object
+	myAudio.src = "sounds/On Rosie Fat Ugly Face.mp3"; // assign the audio file to it
+	myAudio.play(); 
+
 });
+
+
